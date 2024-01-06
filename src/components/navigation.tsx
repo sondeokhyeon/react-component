21a9ef1b9de42da84c37fbd8757553499ebdb98a
sondeokhyeon <sondeokhyeon@gmail.com>
2024-01-06 23:56:15 +0900

    Update : Navigation Style Add

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,23 +2,25 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <aside className="w-48 overflow-y-auto">
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        <div>
-          <h1> Component</h1>
+    <aside className="w-48 overflow-y-auto border-r border-solid border-[#ededed]">
+      <div className="py-2 text-center">
+        <div className="py-8 underline">
+          <Link to="/">Home</Link>
         </div>
-        <div>
-          <ul>
-            <li>
-              <Link to="/component/accordion"> Accodian</Link>
-            </li>
-            <li>
-              <Link to="/component/star-rating"> StarRating</Link>
-            </li>
-          </ul>
+        <div className=" text-center ">
+          <div className="">
+            <h1 className="font-bold">Component</h1>
+          </div>
+          <div className="mt-5">
+            <ul className="flex flex-col gap-1">
+              <li className="underline">
+                <Link to="/component/accordion"> Accodian</Link>
+              </li>
+              <li className="underline">
+                <Link to="/component/star-rating"> StarRating</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </aside>
